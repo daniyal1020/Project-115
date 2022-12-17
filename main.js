2aa54862ca4b27d51f372bd19127563e17856f96
daniyal1020 <107762034+daniyal1020@users.noreply.github.com>
2022-12-17 17:23:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+function preload(){
+
+}
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO)
+    video.size(300, 300);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+
+}
+function draw(){
+    image(video, 0, 0, 300, 300);
+}
+function take_snapshot(){
+    save('filter.png');
+}
+
+function modelLoaded(){
+    console.log('Model is loaded');
+}
+
+function gotPoses(result){
+    if(result.length > 0){
+        console.log(result);
+    }
+}
